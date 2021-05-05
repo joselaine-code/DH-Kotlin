ac4ef5d3e33b36782981b3d12e6649d8b3fcb679
Joselaine Aparecida dos Santos <joselaine.aparecida@gmail.com>
2021-05-05 20:53:05 -0300

    correcao prof

--- src/Exercicios.kt
+++ src/Exercicios.kt
@@ -1,8 +1,10 @@
 fun main() {
-    qualOMaior(10,20,3)
-    parOuImpar(50)
-    imprimirImpares()
-    println(analise(7,10,5,6))
+//    qualOMaior(10,20,3)
+//    parOuImpar(50)
+//    imprimirImpares()
+//    println(analise(7,10,5,6))
+    println(exercicio3(4))
+    exercicio4()
 
 }
 //1)
@@ -26,9 +28,26 @@ fun parOuImpar(n:Int){
     if (n%2 == 0) println("Esse número é par") else println("Esse número é Impar")
 }
 
+//3) CORREÇÃO PROFESSOR
+fun exercicio3(numero:Int): Boolean{
+    return (numero%2 == 0)
+}
+
 //4)
 fun imprimirImpares(){
     for (n in 1..100) if (n%2 != 0){ println(n) }
+}
+//4) CORREÇÃO PROFESSOR
+fun exercicio4(){
+    var cont=0
+    var numero = 1
+    while (cont < 100){
+        if (numero%2 != 0){
+            println(numero)
+            cont++
+        }
+        numero++
+    }
 }
 
 //5)
