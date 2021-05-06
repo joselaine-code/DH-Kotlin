de1c679a2f011053420b4c836a80b37609d59790
Joselaine Aparecida dos Santos <joselaine.aparecida@gmail.com>
2021-05-05 21:41:38 -0300

    termino da aula 02 - classes

--- src/Aula02.kt
+++ src/Aula02.kt
@@ -1,5 +1,28 @@
-fun main() {
+class Aula02 {
+    //Uma classe possui atributos e métodos
 
 }
 
+class Animal (
+    //construtor primário
+    val nome: String,
+    val peso: Int,
+) {
+    //construtor secundário
+    constructor(peso:Int, nome:String, reino: String) : this(nome, peso)
 
+    private val teste1 = "Execução1"
+
+    //primeiro a ser chamado
+    init {
+
+    }
+
+    fun comer() {
+        println("Estou me alimentando")
+    }
+
+    override fun toString(): String {
+        return "Nome: $nome\nPeso: $peso kg"
+    }
+}
---
+++ src/ClasseMain.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val neguinha = Animal("Neguinha", 15)
+
+   println(neguinha)
+}
