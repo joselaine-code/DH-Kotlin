ff35e691bb1d3b24be1462c6c3f6c4a7789bfb3e
Joselaine Aparecida dos Santos <joselaine.aparecida@gmail.com>
2021-05-20 11:44:45 -0300

    alteração no exercicio 4 da aula 12

--- src/Aula12/Exercicios/Exercicio4.kt
+++ src/Aula12/Exercicios/Exercicio4.kt
@@ -11,13 +11,24 @@ class Funcionario(
 }
 
 fun main() {
-    val funcionario = Funcionario("Jaqueline Aparecida", 1611)
-    val funcionario1 = Funcionario("Jaqueline Aparecida", 1611)
+    val listaDeFuncionario = mutableListOf<Funcionario>()
 
-    if (funcionario == funcionario1) {
-        println("São Iguais")
+    val funcionario = Funcionario("Jaqueline Aparecida", 1611)
+    val funcionario1 = Funcionario("Eduardo", 3261)
+    val funcionario2 = Funcionario("Mônica", 1605)
+    val funcionario3 = Funcionario("Alexandre Aparecido", 3265)
+
+    listaDeFuncionario.add(funcionario)
+    listaDeFuncionario.add(funcionario1)
+    listaDeFuncionario.add(funcionario2)
+    listaDeFuncionario.add(funcionario3)
+
+    val funcionario4 = Funcionario("Alexandre Aparecido", 3265)
+
+    if (listaDeFuncionario.contains(funcionario4)) {
+        println("Sim")
     } else {
-        println("São diferentes")
+        println("Não")
     }
 
 }
